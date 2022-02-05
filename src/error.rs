cc4a9168e0761cecb3b5e0aad5424c8062f16f15
Ивкин Станислав <109stas@gmail.com>
2022-02-05 19:17:23 +0300

    X continue...

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,9 @@ pub enum EscrowError {
     /// Invalid instruction
     #[error("Invalid Instruction")]
     InvalidInstruction,
+
+    #[error("Not Rent Exempt")]
+    NotRentExempt
 }
 
 impl From<EscrowError> for ProgramError {
